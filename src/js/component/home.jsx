aaa308dace22b2013d1054d299b02bd5c2288371
rivervil16 <agustinriverovila@gmail.com>
2022-09-14 13:08:07 +0000

    queda la funcionalidad del semaforo. Falta el style

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
-
-	return (
-		<div className="traffic-light">
-			<div className="light red"></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
-		</div>
-	);
+  const [selectedColor, setSelectedColor] = useState("red");
+  return (
+    <div className="traffic-light">
+      <div
+        onClick={() => setSelectedColor("red")}
+        className={"light red" + (selectedColor === "red" ? " glow " : "")}
+      ></div>
+      <div
+        onClick={() => setSelectedColor("yellow")}
+        className={
+          "light yellow" + (selectedColor === "yellow" ? " glow " : "")
+        }
+      ></div>
+      <div
+        onClick={() => setSelectedColor("green")}
+        className={"light green" + (selectedColor === "green" ? " glow " : "")}
+      ></div>
+    </div>
+  );
 }
+
+export default Home;
